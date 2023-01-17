a6985f2b70fa3563c5ce36e19726a0a7dfee0856
Manoj Kumar MC <mkmc@192.168.1.53>
2023-01-17 21:33:20 +0530

    virtual text disabled

--- init.lua
+++ init.lua
@@ -381,6 +381,8 @@ local on_attach = function(_, bufnr)
   end, { desc = "Format current buffer with LSP" })
 end
 
+vim.diagnostic.config({ virtual_text = false })
+
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
