daea0c55c8d63bd9ff70161efd5033bd2d8f6068
Manoj Kumar MC <mkmc@192.168.1.11>
2023-05-28 01:02:38 +0530

    chore: debugger setup

--- init.lua
+++ init.lua
@@ -124,7 +124,7 @@ require("lazy").setup({
     tag = "nightly", -- optional, updated every week. (see issue #1193)
   },
 
- {
+  {
     "akinsho/toggleterm.nvim",
     version = '*',
     config = function()
@@ -137,7 +137,20 @@ require("lazy").setup({
             start_in_insert = true,
         })
     end,
- },
+  },
+
+  {"mfussenegger/nvim-dap"},
+  {"rcarriga/nvim-dap-ui",
+        requires = {"mfussenegger/nvim-dap"} ,
+        config = function()
+            require("dapui").setup({})
+        end,
+  },
+  {"theHamsta/nvim-dap-virtual-text",
+        config = function()
+            require("nvim-dap-virtual-text").setup({})
+        end,
+  },
 
 })
 
@@ -572,3 +585,82 @@ vim.keymap.set("n", "<leader>tfo", "<cmd>NvimTreeFocus<cr>", { desc = "[T]ree [F
 vim.keymap.set("n", "<leader>tff", "<cmd>NvimTreeFindFile<cr>", { desc = "[T]ree [F]ind File" })
 
 require("luasnip.loaders.from_vscode").lazy_load()
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+vim.api.nvim_set_hl(0, "blue",   { fg = "#3d59a1" }) 
+vim.api.nvim_set_hl(0, "green",  { fg = "#9ece6a" }) 
+vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" }) 
+vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" }) 
+vim.fn.sign_define('DapBreakpoint',          { text='', texthl='blue',   linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='blue',   linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected',  { text='', texthl='orange', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapStopped',             { text='', texthl='green',  linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint',            { text='', texthl='yellow', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+
+vim.keymap.set('n', '<F5>', require 'dap'.continue)
+vim.keymap.set('n', '<F10>', require 'dap'.step_over)
+vim.keymap.set('n', '<F11>', require 'dap'.step_into)
+vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint, { desc = "Breakpoint Toggle" })
+
+dap.adapters.python = function(cb, config)
+  if config.request == 'attach' then
+    local port = (config.connect or config).port
+    local host = (config.connect or config).host or '127.0.0.1'
+    cb({
+      type = 'server',
+      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+      host = host,
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  else
+    cb({
+      type = 'executable',
+      command = os.getenv("VIRTUAL_ENV") .. "/bin/python",
+      args = { '-m', 'debugpy.adapter' },
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  end
+end
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        -- return '/usr/bin/python'
+        return os.getenv("VIRTUAL_ENV") .. "/bin/python"
+      end
+    end;
+  },
+}
+
